f4fde92907f2719cc78ab0d907ef40b0240c9f39
SIMON-PC\Simon <gronauer.simon@gmail.com>
2017-11-05 23:55:48 +0100

    added tests

---
+++ src/test/java/org/wahlzeit/services/mailing/EmailServiceTestSuite.java
@@ -0,0 +1,14 @@
+package org.wahlzeit.services.mailing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		EmailServiceTest.class
+})
+
+public class EmailServiceTestSuite {
+
+}
